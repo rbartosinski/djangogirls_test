313b9ffaff0574cf5c8595e157ee31ab33124418
Radek <rbartosinski@wp.pl>
2019-01-14 16:09:21 +0100

    First model

--- newproject/settings.py
+++ newproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#cqr#)4ole91yq)7-@uvs8!#^sh3=sngs$0&ci(q^*&4-61v_1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
